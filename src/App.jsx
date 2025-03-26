b031e1b101c286c7a376797c060fa18eeb5a5f24
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-26 21:51:04 +0600

    last time

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
+import counter from './counter'
 import './App.css'
 
 function App() {
-
+const function handdleloder
   return (
     <>
       
       <h1>Vite + React</h1>
-    
+    <button onClick={handdleloder}></button>
     </>
   )
 }
